9ed6cf1a8f31f99df6f13b73a1343a42249a012f
Varvara Markevich <varvara@Varvaras-MacBook-Pro.local>
2024-06-28 16:28:14 +0400

    Only table

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,6 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import 'emoji-picker-element'
 
 let Hooks = {}
 

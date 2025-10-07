ad69f8fd07cb6d4c1e984d598a04d043f1a01732
slava.markevich <viachaslau.markevich@softswiss.com>
2025-10-07 14:54:41 +0400

    Review fixes

--- mix.exs
+++ mix.exs
@@ -37,7 +37,6 @@ defmodule MomentumHqMixProject do
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:floki, ">= 0.30.0", only: :test},
-      {:mox, "~> 1.0", only: :test},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0.0-rc.0"},
